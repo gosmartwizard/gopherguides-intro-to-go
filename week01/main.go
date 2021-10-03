2107790f3a184ae6225c9146537d150250479ac3
Naveen Reddy Kunareddy <gosmartwizard@gmail.com>
2021-10-03 19:58:45 +0530

    Explored fmt package for verbs

--- week01/main.go
+++ week01/main.go
@@ -11,7 +11,19 @@ func main() {
 	varInteger := 42
 	varBoolean := true
 
+	fmt.Print("\n")
+
+	fmt.Printf("Printing, %T (%q)! \n", varString, varString)
+	fmt.Printf("Printing, %T (%d)! \n", varInteger, varInteger)
+	fmt.Printf("Printing, %T (%t)! \n\n", varBoolean, varBoolean)
+
+	fmt.Printf("Printing, %T (%v)! \n", varString, varString)
+	fmt.Printf("Printing, %T (%v)! \n", varInteger, varInteger)
+	fmt.Printf("Printing, %T (%v)! \n\n", varBoolean, varBoolean)
+
 	fmt.Printf("Printing, %T (%#v)! \n", varString, varString)
 	fmt.Printf("Printing, %T (%#v)! \n", varInteger, varInteger)
-	fmt.Printf("Printing, %T (%#v)! \n", varBoolean, varBoolean)
+	fmt.Printf("Printing, %T (%#v)! \n\n", varBoolean, varBoolean)
+
+	fmt.Printf("Printing, %T (%s)!\n", varString, varString)
 }
