77077a963168426891da4076f7bc11f2e961434e
Naveen Reddy Kunareddy <gosmartwizard@gmail.com>
2021-10-09 09:33:56 +0530

    Added few more test cases for arrays, slices, maps

--- week02/assignment02_test.go
+++ week02/assignment02_test.go
@@ -1,6 +1,9 @@
 package week02
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestArray1(t *testing.T) {
 
@@ -13,7 +16,7 @@ func TestArray1(t *testing.T) {
 
 	for i, v := range act {
 		if act[i] != exp[i] {
-			t.Fatalf("act[%d] : %s is not equal to exp[%d] : %s ", i, v, i, exp[i])
+			t.Errorf("act[%d] : %s is not equal to exp[%d] : %s ", i, v, i, exp[i])
 		}
 	}
 }
@@ -27,8 +30,22 @@ func TestArray2(t *testing.T) {
 		act[i] = v
 	}
 
+	if b := reflect.DeepEqual(act, exp); !b {
+		t.Error("act contents : ", act, " and exp contents : ", exp, "are not equal")
+	}
+}
+
+func TestArray3(t *testing.T) {
+
+	exp := [4]string{"John", "Paul", "George", "Ringo"}
+	act := [4]string{}
+
+	for i, v := range exp {
+		act[i] = v
+	}
+
 	if len(act) != len(exp) {
-		t.Fatalf("len of act : %d is not equal to len of exp : %d ", len(act), len(exp))
+		t.Errorf("len of act : %d is not equal to len of exp : %d ", len(act), len(exp))
 	}
 }
 
@@ -43,7 +60,7 @@ func TestSlice1(t *testing.T) {
 
 	for i, v := range act {
 		if act[i] != exp[i] {
-			t.Fatalf("act[%d] : %s is not equal to exp[%d] : %s ", i, v, i, exp[i])
+			t.Errorf("act[%d] : %s is not equal to exp[%d] : %s ", i, v, i, exp[i])
 		}
 	}
 }
@@ -57,8 +74,22 @@ func TestSlice2(t *testing.T) {
 		act = append(act, v)
 	}
 
+	if b := reflect.DeepEqual(act, exp); !b {
+		t.Error("act contents : ", act, " and exp contents : ", exp, "are not equal")
+	}
+}
+
+func TestSlice3(t *testing.T) {
+
+	exp := []string{"John", "Paul", "George", "Ringo"}
+	var act []string
+
+	for _, v := range exp {
+		act = append(act, v)
+	}
+
 	if len(act) != len(exp) {
-		t.Fatalf("len of act : %d is not equal to len of exp : %d ", len(act), len(exp))
+		t.Errorf("len of act : %d is not equal to len of exp : %d ", len(act), len(exp))
 	}
 }
 
@@ -81,7 +112,7 @@ func TestMap1(t *testing.T) {
 		ve, ok := exp[ka]
 		if ok {
 			if va != ve {
-				t.Fatalf("act[%q] : %s is not equal to exp[%q] : %s ", ka, va, ka, ve)
+				t.Errorf("act[%q] : %s is not equal to exp[%q] : %s ", ka, va, ka, ve)
 			}
 		}
 	}
@@ -98,11 +129,32 @@ func TestMap2(t *testing.T) {
 
 	act := map[string]string{}
 
+
+	for k, v := range exp {
+		act[k] = v
+	}
+
+	if b := reflect.DeepEqual(act, exp); !b {
+		t.Error("act contents : ", act, " and exp contents : ", exp, "are not equal")
+	}
+}
+
+func TestMap3(t *testing.T) {
+
+	exp := map[string]string{
+		"John":   "john@gmail.com",
+		"Paul":   "paul@gmail.com",
+		"George": "george@gmail.com",
+		"Ringo":  "ringo@gmail.com",
+	}
+
+	act := map[string]string{}
+
 	for k, v := range exp {
 		act[k] = v
 	}
 
 	if len(act) != len(exp) {
-		t.Fatalf("len of act : %d is not equal to len of exp : %d ", len(act), len(exp))
+		t.Errorf("len of act : %d is not equal to len of exp : %d ", len(act), len(exp))
 	}
 }
