4c77d918764e3b7e555f82347bfcd4132394b857
Naveen Reddy Kunareddy <gosmartwizard@gmail.com>
2021-10-29 09:57:17 +0530

    Added test cases for errors source file

--- week05/errors_test.go
+++ week05/errors_test.go
@@ -1,6 +1,11 @@
 package week05
 
-import "testing"
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func Test_TableNotFound_1(t *testing.T) {
 	t.Parallel()
@@ -13,7 +18,7 @@ func Test_TableNotFound_1(t *testing.T) {
 		"iPhone": "Iphone6",
 	}
 
-	s := Store{}
+	s := &Store{}
 
 	s.Insert("Mobiles", m1, m2)
 
@@ -29,5 +34,67 @@ func Test_TableNotFound_1(t *testing.T) {
 
 	if ok {
 		t.Fatal("expected: false, got: true")
+	}
+}
+
+func Test_Error_1(t *testing.T) {
+	t.Parallel()
+
+	enr := &errNoRows{
+		table: "Mobiles",
+	}
+
+	err := enr.Error()
+
+	exp := fmt.Sprintf("[%s] no rows found\nquery: ", enr.table)
+
+	if exp != err {
+		t.Fatalf("expected: %#v, got: %#v", exp, err)
+	}
+}
+
+func Test_RowNotFound_1(t *testing.T) {
+	t.Parallel()
+
+	enr := &errNoRows{
+		table: "iPad",
+	}
+
+	_, got := enr.RowNotFound()
+
+	exp := Clauses{}
+
+	if !reflect.DeepEqual(exp, got) {
+		fmt.Printf("expected: %#v, got : %#v", exp, got)
+	}
+}
+
+func Test_AsErrNoRows_1(t *testing.T) {
+	t.Parallel()
+
+	err := errors.New("Other errrr")
+
+	_, ok := AsErrNoRows(err)
+
+	if ok {
+		t.Fatalf("expected false, got: true")
+	}
+}
+
+func Test_AsErrNoRows_2(t *testing.T) {
+	t.Parallel()
+
+	err := &errNoRows{}
+
+	e, ok := AsErrNoRows(err)
+
+	if !ok {
+		t.Fatalf("expected true, got: false")
+	}
+
+	ok = IsErrNoRows(e)
+
+	if !ok {
+		t.Fatalf("expected true, got: false")
 	}
 }
