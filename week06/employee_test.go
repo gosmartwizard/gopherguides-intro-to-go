7ecd05e0e1a9e551d2fa119bcd51a9c67ad3fdd3
Naveen Reddy Kunareddy <gosmartwizard@gmail.com>
2021-11-05 13:04:17 +0530

    Added more test cases

--- week06/employee_test.go
+++ week06/employee_test.go
@@ -2,6 +2,7 @@ package week06
 
 import (
 	"testing"
+	"time"
 )
 
 func Test_Employee_1(t *testing.T) {
@@ -42,4 +43,79 @@ func Test_Employee_3(t *testing.T) {
 	if exp.Error() != err.Error() {
 		t.Fatalf("expected : %#v, got : %#v", exp, err)
 	}
+}
+
+func Test_Employee_4(t *testing.T) {
+	t.Parallel()
+
+	m := NewManager()
+
+	e := Employee(-1)
+
+	go e.work(m)
+
+	go func() {
+		m.Assign(&Product{Quantity: 2})
+	}()
+
+	exp := ErrInvalidEmployee(-1)
+
+	select {
+	case err := <-m.Errors():
+		if exp.Error() != err.Error() {
+			t.Fatalf("expected : %#v, got : %#v", exp, err)
+		}
+	case <-m.Done():
+	}
+}
+
+func Test_Employee_5(t *testing.T) {
+	t.Parallel()
+
+	m := NewManager()
+
+	e := Employee(0)
+
+	go e.work(m)
+
+	go func() {
+		m.Assign(&Product{Quantity: 2})
+	}()
+
+	exp := ErrInvalidEmployee(0)
+
+	select {
+	case err := <-m.Errors():
+		if exp.Error() != err.Error() {
+			t.Fatalf("expected : %#v, got : %#v", exp, err)
+		}
+	case <-m.Done():
+	}
+}
+
+func Test_Employee_6(t *testing.T) {
+	t.Parallel()
+
+	m := NewManager()
+
+	e := Employee(1)
+
+	go e.work(m)
+
+	go func(m *Manager) {
+		m.Assign(&Product{Quantity: 2})
+	}(m)
+
+	var act []CompletedProduct
+
+	go func() {
+		for cp := range m.Completed() {
+			act = append(act, cp)
+			if len(act) == 1 {
+				close(m.Jobs())
+				time.Sleep(time.Millisecond * 1000)
+				close(m.Errors())
+			}
+		}
+	}()
 }
